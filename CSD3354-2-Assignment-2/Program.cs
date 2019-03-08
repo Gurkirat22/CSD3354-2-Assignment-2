b3a53513cee42dbd86342f2e4ce20093344daaf9
visualstudio <visualstudio@122-PC13>
2019-03-08 18:41:20 -0500

    Lab 01

--- CSD3354-2-Assignment-2/Program.cs
+++ CSD3354-2-Assignment-2/Program.cs
@@ -5,11 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace CSD3354_2_Assignment_2
+//Gurkirat Singh C0732169
+//Manvir Singh C0730069
+
 {
-    class Program
+    public class Program
     {
+        public delegate void MyDelegate();
+
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate mydelegate = new MyDelegate(Method1);
+            mydelegate();
+        }
         static void Main(string[] args)
         {
+
         }
     }
 }
